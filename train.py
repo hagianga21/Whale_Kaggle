772d92e5a940b5ad3fbbca219e53d9b86f2abe64
hagianga21 <30545605+hagianga21@users.noreply.github.com>
2018-12-06 13:35:44 +0700

    Add files via upload

--- train.py
+++ train.py
@@ -176,7 +176,8 @@ for epoch in range(args.num_epochs):
         outputs = model(inputs)
         #print(outputs.shape) 32 x 5005 
         #print(labels.shape) 32 x 5005
-        loss = criterion(outputs, labels.long())
+        #loss = criterion(outputs, labels.float())
+        loss = criterion(outputs, torch.max(labels, 1)[1])
         running_loss += loss*inputs.shape[0]
         loss.backward()
         optimizer.step()
